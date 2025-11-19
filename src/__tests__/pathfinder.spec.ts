f728a2847175585dc4980a2fcb381c43eacc76a2
cde123456781 <bjc199@uclive.ac.nz>
2025-11-20 03:48:53 +1300

    Adds pathfinder utils

--- src/__tests__/pathfinder.spec.ts
+++ src/__tests__/pathfinder.spec.ts
@@ -1,52 +1,130 @@
-import { calculateHeuristicCost, decodeMazeString } from '#utils/pathfinder.js';
+import { Node, Pathfinder } from '#utils/pathfinder.js';
 import { describe, expect, it } from 'vitest';
 
 describe('test mazeString function', () => {
     it('should convert mazeString to a valid nested array and return the start and finish positions', () => {
         const answerArray: string[][] = [
-            ['S', ' '],
-            [' ', 'F']
+            ['S', ' ', ' '],
+            [' ', ' ', 'F']
         ];
-        const startCheck: number[] = [0, 0];
-        const finishCheck: number[] = [1, 1];
+        const startCheck: Node = new Node(0, 0, null, 0);
+        const finishCheck: Node = new Node(1, 2, null, 0);
 
-        const [testArray, start, finish] = decodeMazeString('S  F', 2, 2);
+        const pathfinder = new Pathfinder('S    F', 2, 3);
 
-        expect(testArray).toStrictEqual(answerArray);
-        expect(start).toStrictEqual(startCheck);
-        expect(finish).toStrictEqual(finishCheck);
+        expect(pathfinder.mazeArray).toStrictEqual(answerArray);
+        expect(pathfinder.nodesToCheck.poll()).toStrictEqual(startCheck);
+        expect(pathfinder.goalNode).toStrictEqual(finishCheck);
     });
 
     it('should throw an error if the mazeString does not contain a start or finish point', () => {
-        expect(() => decodeMazeString('   F', 2, 2)).toThrowError(
+        expect(() => new Pathfinder('   F', 2, 2)).toThrowError(
             'mazeString does not contain a start or finish'
         );
 
-        expect(() => decodeMazeString('S     ', 2, 3)).toThrowError(
+        expect(() => new Pathfinder('S     ', 2, 3)).toThrowError(
             'mazeString does not contain a start or finish'
         );
 
-        expect(() => decodeMazeString('      ', 2, 3)).toThrowError(
+        expect(() => new Pathfinder('      ', 2, 3)).toThrowError(
             'mazeString does not contain a start or finish'
         );
     });
 
     it('should throw an error if the length of the mazeString does not match the specified height and width', () => {
-        expect(() => decodeMazeString('   F', 2, 9)).toThrowError(
+        expect(() => new Pathfinder('   F', 2, 9)).toThrowError(
             'mazeString does not fit the specified width and height'
         );
     });
 
     it('should throw an error if the mazeString contains an invalid character', () => {
-        expect(() => decodeMazeString('7S F', 2, 2)).toThrowError(
+        expect(() => new Pathfinder('7S F', 2, 2)).toThrowError(
             'mazeString contains an invalid character'
+        );
+    });
+
+    it('should throw an error if the mazeString contains multiple start or finish nodes', () => {
+        expect(() => new Pathfinder('7SSF', 2, 2)).toThrowError(
+            'mazeString has more than one start'
+        );
+
+        expect(() => new Pathfinder('7SFF', 2, 2)).toThrowError(
+            'mazeString has more than one finish'
         );
     });
 });
 
 describe('test calculateHeuristicCost function', () => {
     it('should calculate heuristic correctly', () => {
-        expect(calculateHeuristicCost([1, 2], [4, 8])).toEqual(9);
-        expect(calculateHeuristicCost([6, 4], [1, 3])).toEqual(6);
+        const pathfinder: Pathfinder = new Pathfinder('S  F', 2, 2);
+        expect(
+            pathfinder.calculateHeuristicCost(new Node(0, 0, null, 0))
+        ).toEqual(2);
+        expect(
+            pathfinder.calculateHeuristicCost(new Node(1, 0, null, 0))
+        ).toEqual(1);
+    });
+});
+
+describe('test isGoal method', () => {
+    it('should be able to detect a goal node correctly', () => {
+        const pathfinder1: Pathfinder = new Pathfinder('S  F', 2, 2);
+        const pathfinder2: Pathfinder = new Pathfinder('S      F ', 3, 3);
+        expect(pathfinder1.isGoal(new Node(1, 1, null, 0))).toEqual(true);
+        expect(pathfinder2.isGoal(new Node(2, 1, null, 0))).toEqual(true);
+    });
+
+    it('should be able to detect when a node is not a goal node', () => {
+        const pathfinder1: Pathfinder = new Pathfinder('S  F', 2, 2);
+        const pathfinder2: Pathfinder = new Pathfinder('S      F ', 3, 3);
+        expect(pathfinder1.isGoal(new Node(0, 1, null, 0))).toEqual(false);
+        expect(pathfinder2.isGoal(new Node(1, 1, null, 0))).toEqual(false);
+    });
+});
+
+describe('test getNeighbours method', () => {
+    it('should be able to return a list of valid neighbour nodes', () => {
+        const pathfinder1: Pathfinder = new Pathfinder('S  F', 2, 2);
+        const pathfinder2: Pathfinder = new Pathfinder('S      F ', 3, 3);
+        const neighbours1: Node[] = pathfinder1.getNeighbours(
+            new Node(0, 0, null, 0)
+        );
+        const neighbours2: Node[] = pathfinder2.getNeighbours(
+            new Node(1, 1, null, 0)
+        );
+
+        expect([
+            neighbours1[0].row,
+            neighbours1[0].col,
+            neighbours1[1].row,
+            neighbours1[1].col
+        ]).toStrictEqual([0, 1, 1, 0]);
+
+        const neighbours2Coordinates: number[] = [];
+        neighbours2.forEach((element) => {
+            neighbours2Coordinates.push(element.row);
+            neighbours2Coordinates.push(element.col);
+        });
+
+        expect(neighbours2Coordinates).toStrictEqual([1, 0, 1, 2, 0, 1, 2, 1]);
+    });
+});
+
+describe('test solve method', () => {
+    it('should be able to solve a maze correctly', () => {
+        const pathfinder1: Pathfinder = new Pathfinder('S W     W  F', 4, 3);
+        const pathfinder2: Pathfinder = new Pathfinder('S W    WW  F', 4, 3);
+        const pathfinder3: Pathfinder = new Pathfinder('F  WW     WW  S', 5, 3);
+
+        expect(pathfinder1.solve()).toEqual('SPW P  PW PF');
+        expect(pathfinder2.solve()).toEqual('S WP  PWWPPF');
+        expect(pathfinder3.solve()).toEqual('FPPWWPPPPPWWPPS');
+    });
+
+    it('should return an empty string if maze is not solvable', () => {
+        const pathfinder1: Pathfinder = new Pathfinder('S  WWW     F', 4, 3);
+        const pathfinder2: Pathfinder = new Pathfinder('SWF', 1, 3);
+        expect(pathfinder1.solve()).toEqual('');
+        expect(pathfinder2.solve()).toEqual('');
     });
 });
--- src/utils/pathfinder.ts
+++ src/utils/pathfinder.ts
@@ -1,57 +1,339 @@
-const decodeMazeString = function (
-    mazeString: string,
-    width: number,
-    height: number
-): [string[][], number[], number[]] {
-    const mazeArray: string[][] = [];
-    let start: number[] = [];
-    let finish: number[] = [];
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
+import { PriorityQueue } from 'priority-queue-typescript';
 
-    if (mazeString.length != height * width) {
-        throw new Error(
-            'mazeString does not fit the specified width and height'
-        );
-    } else if (!(mazeString.includes('S') && mazeString.includes('F'))) {
-        throw new Error('mazeString does not contain a start or finish');
+class Node {
+    public get col(): number {
+        return this._col;
     }
 
-    let currentCharIndex = 0;
-    for (let i = 0; i < height; i++) {
-        const rowArray: string[] = [];
-        for (let j = 0; j < width; j++) {
-            const char = mazeString.charAt(currentCharIndex);
-            if (char == ' ' || char == 'S' || char == 'F' || char == 'W') {
-                rowArray.push(char);
+    public set col(newCol: number) {
+        this._col = newCol;
+    }
 
-                if (char == 'S') {
-                    start = [i, j];
-                } else if (char == 'F') {
-                    finish = [i, j];
-                }
+    public get cost(): number {
+        return this._cost;
+    }
+
+    public set cost(newCost: number) {
+        this._cost = newCost;
+    }
+
+    public get count(): number {
+        return this._count;
+    }
+
+    public set count(newCount: number) {
+        this._count = newCount;
+    }
+
+    public get parent(): Node | null {
+        return this._parent;
+    }
+
+    public set parent(parent: Node | null) {
+        this._parent = parent;
+    }
+
+    public get row(): number {
+        return this._row;
+    }
+
+    public set row(newRow: number) {
+        this._row = newRow;
+    }
+
+    private _col: number;
+    private _cost!: number;
+    private _count: number;
+    private _parent: Node | null;
+
+    private _row: number;
+
+    public constructor(
+        row: number,
+        col: number,
+        parent: Node | null,
+        count: number,
+        cost?: number
+    ) {
+        this._row = row;
+        this._col = col;
+        this._parent = parent;
+        this._count = count;
+        if (cost == undefined) {
+            this.cost = 0;
+        } else {
+            this.cost = cost;
+        }
+    }
+}
+
+class Pathfinder {
+    public get currentBestCostMap(): Map<string, Node> {
+        return this._currentBestCostMap;
+    }
+
+    public get goalNode(): Node {
+        return this._goalNode;
+    }
+
+    public set goalNode(node: Node) {
+        this._goalNode = node;
+    }
+
+    public get mazeArray(): string[][] {
+        return this._mazeArray;
+    }
+
+    public set mazeArray(maze: string[][]) {
+        this._mazeArray = maze;
+    }
+
+    public get nodesToCheck(): PriorityQueue<Node> {
+        return this._nodesToCheck;
+    }
+
+    private _currentBestCostMap = new Map<string, Node>();
+    private _goalNode!: Node;
+    private _mazeArray!: string[][];
+    private priorityQueueComparator = (a: Node, b: Node) => {
+        const value =
+            a.cost +
+            this.calculateHeuristicCost(a) -
+            (b.cost + this.calculateHeuristicCost(b));
+
+        if (value == 0) {
+            if (a.count < b.count) {
+                return -1;
             } else {
-                throw new Error('mazeString contains an invalid character');
+                return 1;
+            }
+        }
+
+        return value;
+    };
+
+    private _nodesToCheck = new PriorityQueue<Node>(10, (a: Node, b: Node) =>
+        this.priorityQueueComparator(a, b)
+    );
+
+    private counter = 0;
+
+    public constructor(mazeString: string, width: number, height: number) {
+        let start: Node;
+        [this.mazeArray, start, this.goalNode] = this.decodeMazeString(
+            mazeString,
+            width,
+            height
+        );
+        this.nodesToCheck.add(start);
+        this.currentBestCostMap.set([start.row, start.col].toString(), start);
+    }
+
+    public calculateHeuristicCost = (node: Node): number => {
+        const row_distance = Math.abs(node.row - this.goalNode.row);
+        const col_distance = Math.abs(node.col - this.goalNode.col);
+        return row_distance + col_distance;
+    };
+
+    public encodeMazeString = (): string => {
+        let returnString = '';
+        this.mazeArray.forEach((element) => {
+            element.forEach((value) => {
+                returnString = returnString.concat(value);
+            });
+        });
+
+        return returnString;
+    };
+
+    public getNeighbours = (node: Node): Node[] => {
+        const neighbours: Node[] = [];
+
+        if (node.col - 1 >= 0) {
+            if (this.mazeArray[node.row][node.col - 1] != 'W') {
+                neighbours.push(
+                    new Node(
+                        node.row,
+                        node.col - 1,
+                        node,
+                        this.counter,
+                        node.cost + 1
+                    )
+                );
+
+                this.counter++;
+            }
+        }
+
+        if (node.col + 1 < this.mazeArray[0].length) {
+            if (this.mazeArray[node.row][node.col + 1] != 'W') {
+                neighbours.push(
+                    new Node(
+                        node.row,
+                        node.col + 1,
+                        node,
+                        this.counter,
+                        node.cost + 1
+                    )
+                );
+                this.counter++;
+            }
+        }
+
+        if (node.row - 1 >= 0) {
+            if (this.mazeArray[node.row - 1][node.col] != 'W') {
+                neighbours.push(
+                    new Node(
+                        node.row - 1,
+                        node.col,
+                        node,
+                        this.counter,
+                        node.cost + 1
+                    )
+                );
+            }
+            this.counter++;
+        }
+
+        if (node.row + 1 < this.mazeArray.length) {
+            if (this.mazeArray[node.row + 1][node.col] != 'W') {
+                neighbours.push(
+                    new Node(
+                        node.row + 1,
+                        node.col,
+                        node,
+                        this.counter,
+                        node.cost + 1
+                    )
+                );
+                this.counter++;
+            }
+        }
+
+        return neighbours;
+    };
+
+    public getPath = (node: Node): Node[] => {
+        const path: Node[] = [];
+        let current = node;
+
+        while (current.parent != null) {
+            path.push(current);
+            current = current.parent;
+        }
+
+        return path.reverse();
+    };
+
+    public isGoal = (node: Node): boolean => {
+        return node.col == this.goalNode.col && node.row == this.goalNode.row;
+    };
+
+    public solve = (): string => {
+        let path: Node[];
+        let current: Node | null;
+
+        while (!this.nodesToCheck.empty()) {
+            current = this.nodesToCheck.poll();
+
+            if (this.isGoal(current!)) {
+                path = this.getPath(current!);
+                this.updateMaze(path);
+                return this.encodeMazeString();
             }
 
-            currentCharIndex += 1;
+            const neighbours = this.getNeighbours(current!);
+
+            neighbours.forEach((node) => {
+                const keyString = [node.row, node.col].toString();
+
+                if (!this.currentBestCostMap.has(keyString)) {
+                    this.nodesToCheck.add(node);
+                    this.currentBestCostMap.set(keyString, node);
+                } else {
+                    const recordedValue =
+                        this.currentBestCostMap.get(keyString);
+
+                    if (recordedValue!.cost > node.cost) {
+                        this.currentBestCostMap.set(keyString, node);
+
+                        let found = false;
+                        for (const element of this.nodesToCheck) {
+                            if (element === node) {
+                                found = true;
+                                break;
+                            }
+                        }
+
+                        if (!found) {
+                            this.nodesToCheck.add(node);
+                        }
+                    }
+                }
+            });
         }
-        mazeArray.push(rowArray);
-    }
 
-    return [mazeArray, start, finish];
-};
+        return '';
+    };
 
-const calculateHeuristicCost = function (
-    node: number[],
-    goal: number[]
-): number {
-    const node_x = node[0];
-    const node_y = node[1];
-    const goal_x = goal[0];
-    const goal_y = goal[1];
+    public updateMaze = (updatedNodes: Node[]): void => {
+        updatedNodes.forEach((node) => {
+            if (this.mazeArray[node.row][node.col] != 'F') {
+                this.mazeArray[node.row][node.col] = 'P';
+            }
+        });
+    };
 
-    const x_distance = Math.abs(node_x - goal_x);
-    const y_distance = Math.abs(node_y - goal_y);
-    return x_distance + y_distance;
-};
+    private decodeMazeString = function (
+        mazeString: string,
+        row: number,
+        col: number
+    ): [string[][], Node, Node] {
+        const mazeArray: string[][] = [];
+        let start!: Node;
+        let finish!: Node;
 
-export { calculateHeuristicCost, decodeMazeString };
+        if (mazeString.length != row * col) {
+            throw new Error(
+                'mazeString does not fit the specified width and height'
+            );
+        } else if (!(mazeString.includes('S') && mazeString.includes('F'))) {
+            throw new Error('mazeString does not contain a start or finish');
+        }
+
+        // eslint-disable-next-line @typescript-eslint/no-misused-spread
+        if ([...mazeString].filter((i) => i === 'S').length > 1) {
+            throw new Error('mazeString has more than one start');
+            // eslint-disable-next-line @typescript-eslint/no-misused-spread
+        } else if ([...mazeString].filter((i) => i === 'F').length > 1) {
+            throw new Error('mazeString has more than one finish');
+        }
+
+        let currentCharIndex = 0;
+        for (let i = 0; i < row; i++) {
+            const rowArray: string[] = [];
+            for (let j = 0; j < col; j++) {
+                const char = mazeString.charAt(currentCharIndex);
+                if (char == ' ' || char == 'S' || char == 'F' || char == 'W') {
+                    rowArray.push(char);
+
+                    if (char == 'S') {
+                        start = new Node(i, j, null, 0);
+                    } else if (char == 'F') {
+                        finish = new Node(i, j, null, 0);
+                    }
+                } else {
+                    throw new Error('mazeString contains an invalid character');
+                }
+
+                currentCharIndex += 1;
+            }
+            mazeArray.push(rowArray);
+        }
+
+        return [mazeArray, start, finish];
+    };
+}
+
+export { Node, Pathfinder };
