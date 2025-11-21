fd8b0d8f0480134c8aceb1f2b950a8d239e0243b
cde123456781 <bjc199@uclive.ac.nz>
2025-11-21 17:07:47 +1300

    Format existing files using Prettier

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
-import express from "express";
+import express from 'express';
 const app = express();
-const port = process.env.PORT ?? "9001";
+const port = process.env.PORT ?? '9001';
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
-  console.log("Response sent");
+app.get('/', (req, res) => {
+    res.send('Hello World');
+    console.log('Response sent');
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+    console.log(`Example app listening on port ${port}`);
 });
